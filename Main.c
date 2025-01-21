721517b719b487150ad5ddb63da242e907463ced
Jer <CodeJ3r@gmail.com>
2025-01-20 20:07:33 -0400

    Code revision
    Correctly implemented code that step 3 was asking for.
    
    Better clarification provided

--- Main.c
+++ Main.c
@@ -1,21 +1,37 @@
-/*
-Name: Jeremy Paruch
-Student Number: w0222971
-Date: January 14, 2024
-Instructor: Nadia Gouda
-Class: PROG 2007 Programming In C
-*/
+/*******************************************
+*   Name: Jeremy Paruch                    *
+*   Student Number: w0222971               *
+*   Date: January 14, 2024                 *
+*   Instructor: Nadia Gouda                *
+*   Class: PROG 2007 Programming In C      *
+********************************************
+*****************************************************************************************************************
+* Sources:                                                                                                      *
+* https://labex.io/tutorials/c-circle-area-and-circumference-in-c-123197                                        *
+* https://www.w3schools.com/c/c_constants.php                                                                   *
+* https://www.w3schools.com/c/c_variables_change.php                                                            *
+* https://www.geeksforgeeks.org/g-fact-41-setting-decimal-precision-in-c/                                       *
+* https://www.w3schools.com/c/c_type_conversion.php                                                             *
+* https://www.netacad.com/ C Essentials 1, Module 2.0.2 FLoating-point numbers in real life and in "C" language *
+*****************************************************************************************************************
+********************************
+* Colleagues collaborated with:*
+* David Comeau                 *
+* Judah Csanyi                 *
+* Josh Leslie                  *
+********************************/
 
 #include <stdio.h>
 #include <math.h> // For Step 3
 
 // Step 1: Floating Point Numbers
 
-float Radius = 4;
-double Pi = 3.14159;
+
 
 int main()
-{
+{ 
+    float Radius = 4;
+    const double Pi = 3.14159;
 
     float circumference = 2 * Pi * Radius;
 
@@ -28,9 +44,11 @@ int main()
     /* Explicitly convert the floating point circumference to an  interger and store it in a new variable
     Print both the original floating-point value and the integer-converted value
     */
-    int convert = circumference;
+    int convertCircumference = (int) circumference;
 
-    /* Testing to see this would work before progessing through the assignment
+    /* int convert=circumference; This was misinterepted as to what the assignment was asking for.
+    
+    Testing to see this would work before progessing through the assignment
 
     printf("The Integer-converted Circumference is: %d\n", convert); *Note* Intiially any radius less than 1, causes to be 0 will increase radius past 0.1
     */
@@ -40,18 +58,19 @@ int main()
     //*old code*double planck = 6.6260*pow(10.0,-34.0);This was old code that I found some documentation on thanks to judah and david for the assistance on that.
 
     // Netacads way of using Plancks constant, it does look nicer doesnt it?
-    double planck = 6.62607E-34;
+     const double planck = 6.62607E-34;
     printf("%e\n", planck);
 
-    // Step 4
+    // Step 4 using print f to format and the display the results!
 
-    printf("radius is: %.2f\n", Radius); //  %.2f This drops down up to 2 decimal places!
+    //  %.2f This drops down up to 2 decimal places!
+    printf("radius is of the circle is: %.2f\n", Radius);
 
-    printf("Circumference is: %.3f\n", circumference);
+    printf("The Circumference of the circle is: %.3f\n", circumference); 
 
-    printf("The Integer-converted Circumference is: %d\n", convert);
+    printf(" If we convert the float into an interger, the Integer-converted Circumference is: %d\n", convertCircumference);
 
-    printf("Planks constant in scientific notation is: %e\n", planck);
+    printf(" Here is what Planks constant is in scientific notation: %e\n", planck);
 
     return 0;
 }
